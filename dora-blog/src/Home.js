a0ec09848a0e8a9500b0632c604282ebdf7b6524
Derya Nur <nurderya791@gmail.com>
2023-07-25 16:45:46 +0300

    Fetching data from mock db feature added

--- dora-blog/src/Home.js
+++ dora-blog/src/Home.js
@@ -1,9 +1,42 @@
+import { useState, useEffect } from 'react';
+import BlogList from './BlogList';
+
 const Home = () => {
-    return (  
+    const[blogs, setBlogs]=useState(null);
+    const[isPending,setIsPending]=useState(true);
+    const[error,setError]=useState(null);
+  
+
+    useEffect(()=>{
+        setTimeout(()=>{
+            fetch('http://localhost:8000/blogs')
+            .then(res=>{
+                 if(!res.ok){
+                    throw Error('Could not fetch the data for that resource');
+                 }
+               return res.json();
+            })
+            .then(data=>{
+                setBlogs(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch(err=>{
+                setIsPending(false);
+                setError(err.message);
+            })
+        },1000);
+        
+    }, []);
+
+
+  return (  
         <div className="Home">
-            <h2>Homepage</h2>
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
+           {blogs && <BlogList blogs={blogs} title="All Blogs !" /> }
         </div>
     );
 }
- 
+
 export default Home;
