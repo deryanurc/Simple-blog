1e3f722e45f56e0711a8927d4dc7eae68af8b742
Derya Nur <nurderya791@gmail.com>
2023-08-01 21:10:56 +0300

    Create new blogs page and delete blogs features added

--- dora-blog/src/Home.js
+++ dora-blog/src/Home.js
@@ -1,36 +1,10 @@
-import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-    const[blogs, setBlogs]=useState(null);
-    const[isPending,setIsPending]=useState(true);
-    const[error,setError]=useState(null);
+    const {data:blogs,isPending,error}=useFetch('http://localhost:8000/blogs');
   
-
-    useEffect(()=>{
-        setTimeout(()=>{
-            fetch('http://localhost:8000/blogs')
-            .then(res=>{
-                 if(!res.ok){
-                    throw Error('Could not fetch the data for that resource');
-                 }
-               return res.json();
-            })
-            .then(data=>{
-                setBlogs(data);
-                setIsPending(false);
-                setError(null);
-            })
-            .catch(err=>{
-                setIsPending(false);
-                setError(err.message);
-            })
-        },1000);
-        
-    }, []);
-
-
-  return (  
+return (  
         <div className="Home">
             {error && <div>{error}</div>}
             {isPending && <div>Loading...</div>}
